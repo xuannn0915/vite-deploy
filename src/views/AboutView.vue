c7bccdaddab1c070ac5a0bde835f42edfe64e094
xuan <xuannn0915@gmail.com>
2024-02-10 23:48:35 +0800

    deploy

---
+++ src/views/AboutView.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    {{ data }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    console.log(import.meta.env.VITE_TEXT);
+    const url = import.meta.env.VITE_PATH
+    this.$http
+      .get(url) 
+      .then((res) => { 
+        this.data = res.data.results[0]
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }
+}
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
